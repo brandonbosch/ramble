387b4e5af481f20b4e2f2ab4b705f8c4965ece72
Unknown <brandon@bombbomb.com>
2017-03-23 11:49:41 -0600

    CRLF to LF
    Convert Windows CRLF to LF

--- ramble.sh
+++ ramble.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 RAMBLE_SMTP_TO='default_recipient@mailserver.test'
+RAMBLE_ALT_SMTP_TO='service_desk_ticket_creator@mailserver.test'
 BODY='This is a test body'
 SUBJECT='Notication from Ramble: This is a test'
+MESSAGE_LEVEL='INFO'
 
 while [ $# -gt 0 ]
 do
@@ -9,12 +11,21 @@ do
         -t) RAMBLE_SMTP_TO="$2"; shift;;
         -b) BODY="$2"; shift;;
         -s) SUBJECT="$2"; shift;;
+        -l) MESSAGE_LEVEL="$2"; shift;;
         -*)
             echo >&2 "usage: $0 [-v] [file ...]"
             exit 1;;
-        *)  break;;     # terminate while loop
+        *)  break;;
     esac
     shift
 done
 
-swaks -t "$RAMBLE_SMTP_TO" -s smtp.gmail.com:587 -tls -a LOGIN --header "Subject: $SUBJECT" --body "$BODY" --auth-user "$RAMBLE_SMTP_USER" --auth-password "$RAMBLE_SMTP_PW" -S > /dev/null 2>&1
+if [ $MESSAGE_LEVEL -eq "HIGH" ]; then
+  # priority high, do something different like send a pagerduty alert or an SMS
+else if [ $MESSAGE_LEVEL -eq "MAINT" ]; then
+  # regular maintenance event, could create a ticket or similar etc.
+  swaks -t "$RAMBLE_ALT_SMTP_TO" -s smtp.gmail.com:587 -tls -a LOGIN --header "Subject: $SUBJECT" --body "$BODY" --auth-user "$RAMBLE_SMTP_USER" --auth-password "$RAMBLE_SMTP_PW" -S > /dev/null 2>&1
+else
+  # just send a regular old email
+  swaks -t "$RAMBLE_SMTP_TO" -s smtp.gmail.com:587 -tls -a LOGIN --header "Subject: $SUBJECT" --body "$BODY" --auth-user "$RAMBLE_SMTP_USER" --auth-password "$RAMBLE_SMTP_PW" -S > /dev/null 2>&1
+fi
